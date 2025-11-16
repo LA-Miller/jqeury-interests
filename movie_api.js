4ecd8152823fa7661b78a1126ef0c9d6d2a48461
Luke Miller <88352100+LA-Miller@users.noreply.github.com>
2025-11-16 15:18:50 -0500

    initial commit

---
+++ movie_api.js
@@ -0,0 +1,32 @@
+const movies = [
+    {
+        title: "Inception",
+        director: "Christopher Nolan",
+        category: "Science Fiction",
+        releaseYear: "2010"
+    },
+    {
+        title: "The Shawshank Redemption",
+        director: "Frank Darabont",
+        category: "Drama",
+        releaseYear: "1994"
+    },
+    {
+        title: "The Dark Knight",
+        director: "Christopher Nolan",
+        category: "Action",
+        releaseYear: "2008"
+    },
+    {
+        title: "Parasite",
+        director: "Bong Joon-ho",
+        category: "Thriller",
+        releaseYear: "2019"
+    },
+    {
+        title: "Pulp Fiction",
+        director: "Quentin Tarantino",
+        category: "Crime",
+        releaseYear: "1994"
+    }
+]
